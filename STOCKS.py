bcac53a96cb6215b545234f54e39f817a03f2caf
Carlos Abreu <34762032+carlosabrx@users.noreply.github.com>
2021-01-07 23:34:28 -0500

    Add files via upload

---
+++ STOCKS.py
@@ -0,0 +1,27 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Crypto Price App
+
+Shown are the stock's opening, closing, and volume prices for SPCE!
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'SPCE'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1w', start='2019-11-30', end='2021-01-07')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.write("""
+## Open Price""")
+st.line_chart(tickerDf.Open)
+st.write("""
+## Close Price""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price""")
+st.line_chart(tickerDf.Volume)
